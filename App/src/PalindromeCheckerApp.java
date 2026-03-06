e39a80df0269dafd31c0ac00a475bd71cd7de3cb
DevS005 <devarghasinha2005@gmail.com>
2026-03-06 09:20:24 +0530

    UC1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("  Welcome to Palindrome Checker App  ");
+        System.out.println("======================================");
+
+        System.out.println("Application Version: 1.0");
+        System.out.println("System Initialized Successfully.");
+
+    }
+}
